9586e864008de6e8fa6f2a8557380a844841e466
Pietro Martins de Oliveira <profpietromartins@gmail.com>
2022-11-04 11:02:32 -0300

    partial certificate lambda function

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,31 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 import { document } from "../utils/dynamodbClient"
+import { compile } from "handlebars"
+import * as dayjs from "dayjs"
+
+import { join } from "path"
+import { createWriteStream, readFileSync } from "fs"
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificates.hbs")
+
+  const html = readFileSync(filePath, "utf-8")
+
+  return compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -27,6 +48,20 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ":id": id
     }
   }).promise();
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png")
+
+  const medal = readFileSync(medalPath, "base64")
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal
+  }
+
+  const content = await compileTemplate(data)
 
   return {
     statusCode: 201,
